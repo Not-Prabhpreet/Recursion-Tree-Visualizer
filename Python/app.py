5a12ee543de13587f77902218b796754aa2760e6
Prabh <prabhcms@gmail.com>
2024-08-26 00:14:31 +0530

    adding vercel.json again

--- Python/app.py
+++ Python/app.py
@@ -36,6 +36,6 @@ def visualize_route():
         print(error_message)
         return jsonify({"error": error_message}), 400
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+#if __name__ == '__main__':
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port)
