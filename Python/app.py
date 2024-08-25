608c04cde39f3809dd9200b59cabe9eb5fc790d7
Prabh <prabhcms@gmail.com>
2024-08-25 20:55:31 +0530

    Updated app.py now ready for deployement

--- Python/app.py
+++ Python/app.py
@@ -37,4 +37,5 @@ def visualize_route():
         return jsonify({"error": error_message}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
